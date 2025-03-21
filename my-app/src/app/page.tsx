4943b9665bb9af39c695c84bc4d1372d49ba5bf6
kaif <khansprojectk07@gmail.com>
2025-03-21 23:44:54 +0530

    Link Component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <h1>Welcome Home</h1>
+    return <>
+    <h1>Welcome Home</h1>
+    <Link style={{
+        textDecoration: "none", color: "red", margin: "2rem"
+    }} href="/blog">Blog</Link>
+    <Link style={{
+        textDecoration: "none", color: "red", margin: "2rem"
+    }} href="/products">Products</Link>
+    </>
 }
