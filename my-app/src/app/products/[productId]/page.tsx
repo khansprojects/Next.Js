d39c87c889e7d773e947a69afbf91a93d5cfa125
kaif <khansprojectk07@gmail.com>
2025-03-21 16:10:00 +0530

    Routing Metadata

--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,6 +1,30 @@
-export default async function productDetails({params}: {
-    params: Promise<{productId: string}>
-}) {
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+    params: Promise<{ productId: string }>
+}
+
+// export const generateMetadata = async ({params} : Props) : Promise<Metadata> => {
+//     const id = (await params).productId;
+//     return {
+//         title: `Product ${id}`,
+//     }
+// }
+
+export const generateMetadata = async ({params} : Props) : Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${id}`);
+        }, 100);
+    });
+    return {
+        title: `Product ${title}`,
+    }
+}
+
+export default async function productDetails({params}:  Props) {
     const productId = (await params).productId;
     return <h1>Details about product {productId}</h1>
 }
