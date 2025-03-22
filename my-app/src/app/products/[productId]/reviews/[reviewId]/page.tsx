12455b0d6c534c192d163dc607cb1c882482854a
kaif <khansprojectk07@gmail.com>
2025-03-22 15:02:45 +0530

    Navigating Programmatically

--- my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,12 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function ProductReview({params} : {
     params: Promise<{productId:  string; reviewId: string}>
 }) {
     const {productId, reviewId} = await params;
     if (parseInt(reviewId) > 100) {
-        notFound();
+        // notFound();
+        redirect("/products");
     }
     return <h1>Review {reviewId} for product {productId}</h1>
 }
