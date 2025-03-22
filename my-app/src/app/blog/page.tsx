c0affebd2697e31a4f9d28d33e633d5df749ea38
kaif <khansprojectk07@gmail.com>
2025-03-22 15:22:16 +0530

    Loading UI

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import { resolve } from "path"
 
 export const metadata: Metadata = {
     title: {
@@ -7,6 +8,9 @@ export const metadata: Metadata = {
     }
 }
 
-export default function Blog() {
+export default async function Blog() {
+    await new Promise(resolve => {
+        setTimeout(() => {resolve("intentional delay")}, 2000)
+    })
     return <h1>My Blog</h1>
 }
